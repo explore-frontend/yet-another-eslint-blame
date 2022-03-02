72e59510d3590cf7bd9b3a0bd408154e73ab4e29
wangwenlu <wangwenlu@kuaishou.com>
2022-03-02 16:37:20 +0800

    initial commit

--- scripts/build.js
+++ scripts/build.js
@@ -2,10 +2,12 @@ const esbuild = require('esbuild');
 
 esbuild.build({
     entryPoints: [
-        './src/index.ts'
+        './src/index.ts',
+        './src/cli.ts'
     ],
-    bundle: true,
+    bundle: false,
     format: 'cjs',
+    platform: 'node',
     sourcemap: true,
     outdir: './dist',
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,220 @@
-console.log('hello world');
+import type * as eslint from 'eslint';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers'
+import getStdin from 'get-stdin'
+import * as fs from 'fs';
+import * as path from 'path';
+import { blameLine } from 'git-blame-line';
+
+enum OutputFormat {
+    Json = 'json',
+    Markdown = 'markdown'
+}
+
+enum Severity {
+    Off = 0,
+    Warn = 1,
+    Error = 2
+}
+
+interface BlameInfo {
+    filePath: string
+    line: number
+    author: string
+    email: string
+    ruleId: string | null
+    message: string
+    isWarning: boolean
+    time: string
+}
+
+interface Args {
+    format?: OutputFormat
+    warn?: boolean
+    input?: string
+    output?: string
+}
+
+function isDef<T>(v: T): v is NonNullable<T> {
+    return v !== undefined && v !== null
+}
+
+function assertDef<T>(v: T, message?: string): asserts v is NonNullable<T> {
+    if (!isDef(v)) {
+        throw new Error(message ?? 'Must be defined');
+    }
+}
+
+function assert(v: unknown, message?: string): asserts v {
+    if (!v) {
+        throw new Error(message ?? 'Assert failed')
+    }
+}
+
+function filePathWithLineFactory (filePath: string, line: number) {
+    return `${filePath}:${line}`
+}
+
+function jsonFormatter(infos: BlameInfo[]): string {
+    return JSON.stringify(infos)
+}
+
+function writeTodoListStatus (done: boolean) {
+    return done ? 'x' : ' '
+}
+
+function writeTodoList(...items: string[]) {
+    return items.join('\n')
+}
+
+function writeTodoListItem(status: boolean, content: string) {
+    return `- [${writeTodoListStatus(status)}] ${content}`
+}
+
+function writeLink(text: string, link: string) {
+    return `[${text}](${link})`
+}
+
+function writeAt(name: string) {
+    return `@${name}`
+}
+
+function writeFilePathAndLine (filePath: string, line: number) {
+    return `${filePath}#L${line}`
+}
+
+function writeRelativePath (path: string) {
+    return `./${path}`
+}
+
+function writeContent (...contents: string[]) {
+    return contents.join(' ')
+}
+
+function markdownFormatter(infos: BlameInfo[]) {
+    return writeTodoList(
+        ...infos.map(info => {
+            return writeTodoListItem(false, writeContent(
+                writeLink(
+                    writeFilePathAndLine(info.filePath, info.line),
+                    writeRelativePath(writeFilePathAndLine(info.filePath, info.line))
+                ),
+                writeAt(info.author)
+            ))
+        })
+    )
+}
+
+export async function blame (content: string, argv: Omit<Args, 'input' | 'output'>) {
+    const results = JSON.parse(content) as eslint.ESLint.LintResult[];
+    const format = argv.format ?? OutputFormat.Json;
+    const includeWarn = argv.warn ?? false;
+
+    const infos: BlameInfo[] = [];
+    for (const result of results) {
+        const filePath = result.filePath;
+        
+        for (const message of result.messages) {
+            if (message.severity === Severity.Error || includeWarn && message.severity === Severity.Warn) {
+                const filePathWithLine = filePathWithLineFactory(filePath, message.line);
+                const blameResult = await blameLine(filePathWithLine)
+
+                infos.push({
+                    filePath,
+                    line: message.line,
+                    author: blameResult.author,
+                    email: blameResult.authorMail,
+                    time: blameResult.authorTime.toString(),
+                    ruleId: message.ruleId,
+                    isWarning: message.severity === Severity.Warn,
+                    message: message.message
+                })
+            }
+        }
+    }
+
+    if (format === OutputFormat.Json) {
+        return jsonFormatter(infos)
+    }
+    else {
+        assert(format === OutputFormat.Markdown);
+        return markdownFormatter(infos);
+    }
+}
+
+function handlerFactory (data?: string) {
+    return async function handler (argv: yargs.Arguments<Args>) {
+        const content = await getData();
+        const result = await blame(content, argv);
+        if (argv.output) {
+            const dirname = path.dirname(argv.output);
+            if (!fs.existsSync(dirname)) {
+                await fs.promises.mkdir(dirname, { recursive: true });
+            }
+
+            await fs.promises.writeFile(argv.output, result);
+        } else {
+            console.log(result);
+        }
+
+        async function getData () {
+            if (isDef(data)) {
+                return data
+            }
+            const input = argv.input
+            assertDef(input);
+
+            const buffer = await fs.promises.readFile(input);
+            return buffer.toString()
+        }
+    }
+}
+
+export async function main () {
+    const data = await getStdin();
+    const isReadData = !!data.length;
+    const inputArg = !isReadData ? '<input>' : undefined
+
+    yargs(hideBin(process.argv))
+        .strict()
+        .command<Args>({
+            command: ['$0', inputArg, '[options]'].filter(isDef).join(' '),
+            describe: '',
+            builder: (yargs: yargs.Argv<Args>) => {
+                if (isReadData) {
+                    return yargs
+                }
+
+                return yargs.positional('input', {
+                    describe: 'input file path',
+                    type: 'string',
+                    normalize: true
+                }).epilog('Happy hack')
+                
+            },
+            handler: handlerFactory(data),
+        })
+        .option('f', {
+            alias: 'format',
+            describe: 'Output format',
+            type: 'string',
+            choices: [
+                OutputFormat.Json,
+                OutputFormat.Markdown
+            ]
+        })
+        .option('o', {
+            alias: 'output',
+            describe: 'Output file path',
+            type: 'string',
+            normalize: true
+        })
+        .option('w', {
+            alias: 'warn',
+            describe: 'Includes warn message',
+            type: 'boolean'
+        })
+        .version().alias('v', 'version')
+        .showHelpOnFail(true, 'Specify --help for available options')
+        .help('h').alias('h', 'help').argv
+}
